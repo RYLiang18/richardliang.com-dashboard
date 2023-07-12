d0ad647ed84de5053c6f60c9cdbb5b2de6ccd93f
rliang88 <rliang@vmware.com>
2023-07-12 14:19:02 -0700

    [PWAD-13] removed old homepage update about me

--- flask_app/homepage/routes.py
+++ flask_app/homepage/routes.py
@@ -72,22 +72,3 @@ def update_description():
         form=description_update_form,
         title="Homepage - Update Description",
     )
-
-
-# @homepage_blueprint.route("/update_about_me", methods=["GET", "POST"])
-# @login_required
-# def update_about_me():
-#     homepage_details = HomepageDetails.objects(owner=current_user).first()
-
-#     about_me_update_form = AboutMeUpdateForm(about_me=homepage_details.about_me)
-
-#     if about_me_update_form.validate_on_submit():
-#         homepage_details.update(about_me=about_me_update_form.about_me.data)
-
-#         return redirect(url_for("homepage.index"))
-
-#     return render_template(
-#         "update_about_me.html",
-#         form=about_me_update_form,
-#         title="Homepage - Update About Me",
-# )
