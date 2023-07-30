5e6d4bbd940e28af373442558a0a7bbe62825ee3
rliang88 <rliang@vmware.com>
2023-07-29 23:26:09 -0700

    [PWAD-21] created projects model

--- flask_app/projects/routes.py
+++ flask_app/projects/routes.py
@@ -1,18 +1,20 @@
-from flask import (
-    Blueprint,
-    render_template
-)
-from flask_login import(
-    login_required,
-    current_user
+from flask import Blueprint, render_template
+from flask_login import current_user, login_required
+
+from flask_app.models import Project
+
+projects_blueprint = Blueprint(
+    "projects", __name__, url_prefix="/projects", template_folder="./templates"
 )
 
-
-projects_blueprint = Blueprint("projects", __name__, url_prefix="/projects", template_folder='./templates')
 
 @projects_blueprint.route("/")
 @login_required
 def index():
+    projects = Project.objects()
+
     return render_template(
-        "projects.html", title=f"{current_user.username}\'s projects"
+        "projects.html",
+        title=f"{current_user.username}'s experiences",
+        projects=projects,
     )
