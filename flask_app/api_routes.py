74c7795a30644ea03b9cb45b16ae93ebe4e4ede0
rliang88 <rliang@vmware.com>
2023-09-30 15:08:47 -0700

    learned jsonify to send json payload response

--- flask_app/api_routes.py
+++ flask_app/api_routes.py
@@ -1,5 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+from flask_app.models import HomepageDetails, load_user
 
 api_blueprint = Blueprint("api", __name__, url_prefix="/api")
 
-# TODO
+
+@api_blueprint.route("/view_homepage_details/<username>", methods=["GET"])
+def view_homepage_details(username):
+    homepage_details = HomepageDetails.objects(owner=load_user(username)).first()
+
+    if homepage_details is None:
+        return jsonify(
+            {
+                "error": f"homepage details document not found. The user [{username}] may not exist, or they may not have a homepage yet"
+            }
+        )
+
+    resp = {
+        "full_name": homepage_details.full_name,
+        "email": homepage_details.email,
+        "image_link": homepage_details.image_link,
+        "description": homepage_details.description,
+        "long_description": homepage_details.long_description,
+    }
+
+    return jsonify(resp)
